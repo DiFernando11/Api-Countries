48b381d74cfd3addaac6879241cd569edee3c024
DiFernando11 <102831050+DiFernando11@users.noreply.github.com>
2022-12-12 08:47:14 -0500

    deploy v4.0

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ const { informationApiToDB } = require("./src/Controllers/util");
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
-    //  informationApiToDB();
+      informationApiToDB();
   });
 });
